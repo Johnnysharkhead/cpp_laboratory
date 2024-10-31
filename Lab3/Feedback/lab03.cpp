b5301e8e1cb446f70a12ea160a3961fe21a0c953
ketjuk <74028692+ketjuk@users.noreply.github.com>
2024-10-31 17:31:44 +0100

    Add files via upload
    他说的empty file的意思我也没懂，我把test.cc后面几个emptyList测试给加上了，不知道他是不是这个意思；
    改了一点lab03.h lab03.cpp的头文件；
    删掉了多余的delete（没有new就不用delete）。

--- Lab3/Feedback/lab03.cpp
+++ Lab3/Feedback/lab03.cpp
@@ -1,74 +1,49 @@
-#include "lab03.h"
+﻿#include "lab03.h"
 #include <iostream>
-
-// TODO: Complementary work: Do not iterate through the list unnecessarily 
-// Comment: When the listlength() function is called you iterate through the
-// whole list, when used in a loop you do this n² times. -- CHECK
-
-// TODO: Complementary work: Initialize variables with {} not = -- CHECK
-
-// TODO: Complementary work: Code duplication. Can you perhaps 
-// reuse the code from the copy constructor by creating a local copy 
-// of other? Same goes for move. -- CHECK (partly)
-
-// TODO: Complementary work: The list should not print anything unless
-// explicitly told to. -- CHECK
-
-// TODO: Complementary work: You have some redundant deletes.
-
-
 using namespace std;
+
+
 // In this programme, we give the following name to elements in a link list:
 // Head -> first node(index 0) -> second node(index 1) ... -> null 
-myList::myList() : head {nullptr}, length(0) {}
 
-// Optimize the code duplication, reuse this function and called by 'Copy constructor' and 'Copy assginment opearator'
-void myList::copyList(const myList& other){
-    // If the list 'other' is empty
-    if(other.head == nullptr){
-        head = nullptr;
-    }
-    else{
-    //Copy the first node
-    head = new Node(other.head->data);
-    //Copy rest of nodes in list "other"
-    Node* tempNode = head; // Pointer current point to the first node of "newList"
-    Node* k_ptr = other.head->next; // Pointer k_ptr point to the second node of "other" list
-    while(k_ptr != nullptr){
-        tempNode->next = new Node{k_ptr->data};
-        k_ptr = k_ptr->next;
-        tempNode = tempNode->next;
-    }
-    tempNode = nullptr; // the last node of "newList" point to NULL
-    }
-}
-
-// Copy constructor(deep copy)
-myList::myList(const myList& other){
-    copyList(other);
-}
-
-// Copy assginment opearator(deep copy) 
-myList& myList::operator= (const myList& other){
-    if (this == &other) {return *this;}
-    else{
-        copyList(other); // reuse the code 
-        return *this;
-    }
-}
+myList::myList() : head{ nullptr } {}
 
 // Move constructor
-myList::myList(myList&& other){
+myList::myList(myList&& other) noexcept {
     head = other.head;// Head node point to the first node of the list that being moved
     other.head = nullptr; // Free other's head node, avoid memory leak
 }
 
+// Copy constructor(deep copy)
+myList::myList(const myList& other) {
+    // Copy an empty list
+    if (other.head == nullptr) {
+        head = nullptr;
+    }
+    else {
+        // Copy the first node
+        head = new Node{ other.head->data };
+        // Copy other nodes
+        Node* tempNode = head; // Initialize pointer tempNode which point to the first node of "this"
+        Node* k_ptr = other.head->next; // Initialize pointer k_ptr which point to the second node of "other"
+
+        while (k_ptr != nullptr)
+        {
+            tempNode->next = new Node{ k_ptr->data };
+            k_ptr = k_ptr->next;
+            tempNode = tempNode->next;
+        }
+        tempNode = nullptr;
+    }
+}
+
 // Move assginment operator
-myList& myList::operator= (myList&& other){
-    if(this == &other){
+myList& myList::operator= (myList&& other) noexcept {
+    std::cout << "Shallow copy called" << std::endl;
+    if (this == &other) {
         return *this; // Judge whether they are same
     }
-    else{
+    else {
         this->deleteEveryNode(); // Release all values of the current list except for the head node
         head = other.head;
         other.head = nullptr; // Avoid memory leak
@@ -76,13 +51,45 @@ myList& myList::operator= (myList&& other){
     return *this;
 }
 
+// Copy assginment opearator(deep copy) 
+myList& myList::operator= (const myList& other) {
+
+    std::cout << "Deep copy called" << std::endl;
+    // Handle the exception
+    if (this == &other) {
+        return *this;
+    }
+
+    if (other.head == nullptr) {
+        head = nullptr;
+        return *this;
+    }
+    else {
+        // Copy the first node
+        this->deleteEveryNode(); // Avoid memory leaks
+        head = new Node{ other.head->data };
+
+        // Copy rest of nodes
+        Node* current = head; // Pointer current point to the first node of newList
+        Node* k_ptr = other.head->next; // Pointer k_ptr point to the second node of "other" list
+
+        while (k_ptr != nullptr) {
+            current->next = new Node{ k_ptr->data }; // Create from the second node to the last node
+            current = current->next;
+            k_ptr = k_ptr->next;
+        }
+        current = nullptr;
+        return *this;
+    }
+}
+
 
 // Call by Destructor and Move assignment operator
-void myList:: deleteEveryNode(){
+void myList::deleteEveryNode() {
     Node* tempNode = head;
     while (tempNode != nullptr)
     {
-        Node* next = tempNode -> next;
+        Node* next = tempNode->next;
         delete tempNode;
         tempNode = next;
     }
@@ -91,160 +98,166 @@ void myList:: deleteEveryNode(){
 
 myList:: ~myList() {
     deleteEveryNode();
-    //sstd::cout << "\n***The list has been destroyed by the destructor*** \n" << std::endl; 
+    std::cout << "\n***The list has been destroyed by the destructor*** \n" << std::endl;
 }
 
-void myList::insert(int addValue){
+
+void myList::insert(int addValue) {
 
     // "newNode" is a pointer that point to the Node in Heap momory
     // "tempNode" is a pointer we use it as a searcher that can navigate any location of the list
-    Node* newNode = new Node{addValue} ; 
+    Node* newNode = new Node{ addValue };
 
     // Condition 1: insert the Node at the first place
-    if (head == nullptr || head->data > newNode->data ) { 
+    if (head == nullptr || head->data > newNode->data) {
         newNode->next = head;
         head = newNode;
         newNode = nullptr;
     }
-    else{
-    // Condition 2: insert the Node either at the end of the list or between any two Node in the list
-        Node* tempNode = head; 
-        
+    else {
+        // Condition 2: insert the Node either at the end of the list or between any two Node in the list
+        Node* tempNode = head;
+
         // move the tempNode to the location we want
         // stop moving the tempNode until the Node after the Node which tempNote point to is bigger than insert Node
-        while (tempNode->next != nullptr && tempNode->next->data < newNode->data) { 
+        while (tempNode->next != nullptr && tempNode->next->data < newNode->data) {
             tempNode = tempNode->next;
         }
         // although tempNode->next is null, set newNode->next as null and it will be the last Node of the list
-        newNode->next = tempNode->next; 
+        newNode->next = tempNode->next;
         tempNode->next = newNode;
-        newNode = nullptr; 
+        newNode = nullptr;
     }
-    length++; // update the length of list           
 }
 
-void myList::removebyValue(int removeValue){
+void myList::removebyValue(int removeValue) {
     Node* tempNode = head;
-    
+
     // if the the node we want to delete is the first node
     while (tempNode != nullptr && tempNode->data == removeValue)
     {
         head = tempNode->next;
         delete tempNode;
-        length--; // update of the length of list
         return;
     }
 
     // if the the node we want to delete is other node except the first node
     while (tempNode != nullptr && tempNode->next != nullptr)
     {
-        if(tempNode->next->data == removeValue){
+        if (tempNode->next->data == removeValue) {
             Node* k = tempNode->next;
             tempNode->next = k->next;
             delete k; // prevent memory leak
-            length--; // update of the length of list
             return;
         }
-        else{
-            tempNode = tempNode -> next;
+        else {
+            tempNode = tempNode->next;
         }
     }
 
     // handle the exception
     std::cout << "Sorry, there is no match node with the value you input in the list. \n" << std::endl;
     throw std::out_of_range("Value out of range");
-    return;  
+    return;
+
 }
 
-void myList::removebyIndex(int removeIndex){
+void myList::removebyIndex(int removeIndex) {
 
     // Ensure the index is reasonable
-    if (removeIndex < 0 || removeIndex > (listLength()-1) ){ // handle the exception
+    if (removeIndex < 0 || removeIndex >(listLength() - 1)) { // handle the exception
         std::cout << "The index you input is invalid, please try again. \n" << std::endl;
         throw std::out_of_range("Index out of range");
         return;
     }
 
-    else{
+    else {
         Node* tempNode = head;
         // if we want to delete the first Node of the list
-        if (removeIndex == 0){
-            head = tempNode -> next;
+        if (removeIndex == 0) {
+            head = tempNode->next;
             delete tempNode;
-            length--; // update of the length of list
             return;
         }
         // if we want to delete the other Node of the list
-        for (int i = 1; i < (removeIndex) ; i++){
-            tempNode =  tempNode -> next; // move the pointer to the Node before the Node we want to delete;
+        for (int i = 1; i < (removeIndex); i++) {
+            tempNode = tempNode->next; // move the pointer to the Node before the Node we want to delete;
         }
         Node* k = tempNode->next; // Now, pointer k point to the node we want to delete
-        tempNode -> next = k -> next; // if we want to delete the last node, then k->next = null
-        delete k; 
-        length--; // update of the length of list
-        return;  
+        tempNode->next = k->next; // if we want to delete the last node, then k->next = null
+        delete k;
+        return;
     }
 }
 
-void myList::printList() const{
+void myList::printList() const {
 
     Node* tempNode = head;
     std::cout << "The current list: ";
 
-    if (tempNode==nullptr)
+    if (tempNode == nullptr)
     {
         std::cout << "NULL";
     }
 
-    while(tempNode != nullptr)
+    while (tempNode != nullptr)
     {
         std::cout << tempNode->data << " -> ";
-        tempNode = tempNode -> next;
+        tempNode = tempNode->next;
     }
     std::cout << "null" << std::endl;
-    std::cout << "The amount of nodes in the current list is: " << this->listLength() << "\n" << std::endl;   
+
+    std::cout << "The amount of nodes in the current list is: " << this->listLength() << "\n" << std::endl;
 }
-std::string myList::printTest() const{
+
+std::string myList::printTest() const {
     string test{};
     Node* tempNode = head;
 
-    if (tempNode==nullptr){
+    if (tempNode == nullptr) {
         test = "NULL";
         return test;
     }
 
-    while (tempNode!=nullptr)
+    while (tempNode != nullptr)
     {
-      test.append(std::to_string(tempNode->data));
-      tempNode = tempNode->next;
+        test.append(std::to_string(tempNode->data));
+        tempNode = tempNode->next;
     }
     return test;
 }
 
-int myList::at(int nodeIndex) const{
+int myList::at(int nodeIndex) const {
     // handle the exception
-    if ( nodeIndex < 0 || nodeIndex > (listLength()-1) ){
+    if (nodeIndex < 0 || nodeIndex >(listLength() - 1)) {
         std::cout << "The index you input is invalid, please try again. \n" << std::endl;
         return -1;
     }
 
-    Node* tempNode = head ;
+    Node* tempNode = head;
 
-    if ( nodeIndex == 0 ){
+    if (nodeIndex == 0) {
         std::cout << "The value of the NO.1 node is: " << head->data << std::endl;
         return head->data;
     }
-    for (int i = 1; i <= nodeIndex; i++){
+    for (int i = 1; i <= nodeIndex; i++) {
         tempNode = tempNode->next;
     }
-    std::cout << "The value of the NO."<< nodeIndex << " node is: " << tempNode->data << " \n" << std::endl;
-    return tempNode->data;        
+    std::cout << "The value of the NO." << nodeIndex << " node is: " << tempNode->data << " \n" << std::endl;
+    return tempNode->data;
 }
 
-// User has to get the length of list by call this instead of the private member variable 'length'
-int myList::listLength() const{ 
-    if (head == nullptr) return 0;
-    else return length;
+// If there is an empty list, the length of the list is 0.
+int myList::listLength() const {
+    Node* listLength = head;
+    if (head == nullptr) { // an empty list
+        return 0;
+    }
+
+    int count{ 0 };
+    while (listLength->next != nullptr) {
+        ++count; // the amount of nodes in the list is count+1
+        listLength = listLength->next;
+    }
+    return count + 1;
 }
-
-
--- Lab3/Feedback/test.cc
+++ Lab3/Feedback/test.cc
@@ -1,11 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "lab03.h"
 #include "catch.hpp"
-
-// TODO: Complementary work: Test cases insufficient, what if a list is 
-// empty for all functions? -- CHECK
-
-// TODO: Complementary work: unnecessary include  -- CHECK 
+#include <random>
 
 // In this programme, we give the following name to elements in a link list:
 // Head -> first node(index 0) -> second node(index 1) ... -> null
@@ -15,9 +11,9 @@
 //=======================================================================
 
 TEST_CASE( "CRUD of link list" ) { // No memory leak detected
-    
     myList list;
-
+    CHECK(list.listLength() == 0);
+    
     list.insert(3), list.insert(2), list.insert(5), list.insert(9), list.insert(8);
     CHECK(list.listLength() == 5);
     CHECK(list.printTest() == "23589");
@@ -38,34 +34,20 @@ TEST_CASE( "CRUD of link list" ) { // No memory leak detected
     CHECK(list.at(2) == -1); // Test Throws Exceptions
 }
 
-TEST_CASE( "All about empty list"){
-    myList emptyList;
-    CHECK(emptyList.listLength() == 0); // test the length of an empty list
-    CHECK_THROWS_AS(emptyList.removebyValue(0), std::out_of_range); // remove a node by value in an empty list
-    CHECK_THROWS_AS(emptyList.removebyIndex(0), std::out_of_range); // remove a node by index in an empty list
+TEST_CASE( "MOVE Constructor of link list" ) { // No memory leak detected
 
-    // test Copy Constructor of an empty list
-    myList a = emptyList;
-    CHECK(a.printTest() == "NULL");
+    myList list;
+    list.insert(8), list.insert(4), list.insert(2), list.insert(6);
 
-    // test Move Constructor of an empty list
-    myList b (std::move(emptyList));  
-    CHECK(b.printTest() == "NULL");
+    myList listmove (std::move(list));
+    CHECK(listmove.printTest() == "2468");
+    CHECK(list.listLength() == 0); // Confirm that we release all resources of list
 
-    // test Move Operator Assignment ( List c = move(empty list) )
-    myList c; 
-    c.insert(1), c.insert(2), c.insert(3);
-    c = std::move(emptyList);
-    CHECK(c.printTest() == "NULL");
-    
-    // test Copy Operator Assignment ( List c = empty list )
-    emptyList.deleteEveryNode();
-    c = emptyList;
-    CHECK(c.printTest() == "NULL");
-    emptyList.insert(4);
-    CHECK(c.printTest() == "NULL");
-}
+    myList emptylist; // Test empty list
+    myList k (std::move(emptylist));
+    CHECK(k.printTest() == "NULL");
 
+} 
 
 TEST_CASE( "COPY Constructor of link list" ) { // No memory leak detected
 
@@ -74,12 +56,34 @@ TEST_CASE( "COPY Constructor of link list" ) { // No memory leak detected
 
     myList listcopy = list;
     CHECK(listcopy.printTest() == "2468");
-
+    
     list.removebyValue(2);
     CHECK(list.printTest() == "468");
     CHECK(listcopy.printTest() == "2468"); // listcopy does not change with list
 
+    myList emptylist; // Test empty list
+    myList k(emptylist) ;
+    CHECK(k.printTest() == "NULL");
+
 }
+
+TEST_CASE( "MOVE assignment operator of link list" ) { // No memory leak detected
+
+    myList a;
+    a.insert(1), a.insert(2), a.insert(3), a.insert(4);
+
+    myList b;
+    b.insert(6), b.insert(7), b.insert(8);
+
+    a = std::move(b);
+
+    CHECK(a.printTest() == "678");
+    CHECK(b.listLength() == 0); // Confirm that we release all resources of list b
+
+    myList emptylist; // Test empty list
+    myList k = std::move(emptylist);
+    CHECK(k.printTest() == "NULL");
+} 
 
 TEST_CASE( "COPY assignment operator of link list" ) { // No memory leak detected
 
@@ -96,28 +100,7 @@ TEST_CASE( "COPY assignment operator of link list" ) { // No memory leak detecte
     CHECK(b.printTest() == "6789");
     CHECK(a.printTest() == "678"); // Confirm list a does not change with b
 
-} 
-
-TEST_CASE( "MOVE assignment operator of link list" ) { // No memory leak detected
-    myList a;
-    a.insert(1), a.insert(2), a.insert(3), a.insert(4);
-
-    myList b;
-    b.insert(6), b.insert(7), b.insert(8);
-
-    a = std::move(b);
-
-    CHECK(a.printTest() == "678");
-    CHECK(b.listLength() == 0); // Confirm that we release all resources of list b
-} 
-
-TEST_CASE( "MOVE Constructor of link list" ) { // No memory leak detected
-
-    myList list;
-    list.insert(8), list.insert(4), list.insert(2), list.insert(6);
-
-    myList listmove (std::move(list));
-    CHECK(listmove.printTest() == "2468");
-    CHECK(list.listLength() == 0); // Confirm that we release all resources of list
-
+    myList emptylist; // Test empty list
+    myList k = emptylist;
+    CHECK(k.printTest() == "NULL");
 } 
