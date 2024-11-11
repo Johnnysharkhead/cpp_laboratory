80101e7c857a2b4137088121a67928b1dc3ac399
ketjuk <74028692+ketjuk@users.noreply.github.com>
2024-11-11 13:39:01 +0100

    Add files via upload

--- Lab4/lab04.h
+++ Lab4/lab04.h
@@ -4,25 +4,34 @@
 
 class Component {
 public:
+	virtual double computingVoltage() = 0;
+	virtual double computingCurrent() = 0;
 protected:
-	int m_terminal_1;
-	int m_terminal_2;
+	double m_terminal_1;
+	double m_terminal_2;
 };
 
 class Battery: public Component {
 public:
+	double computingVoltage() override;
+	double computingCurrent() override;
 private:
-	int m_voltage;
+	double m_voltage;
 };
 
 class Resistor : public Component {
 public:
+	double computingVoltage() override;
+	double computingCurrent() override;
 private:
 	double m_resistance;
 };
 
 class Capacitor : public Component {
 public:
+	double computingVoltage() override;
+	double computingCurrent() override;
 private:
 	double m_capacitance;
+	double m_storedVoltage;
 };
