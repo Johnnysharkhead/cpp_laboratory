fcaccccdff0db7a87017f60d055c3e78f0c661ce
ketjuk <74028692+ketjuk@users.noreply.github.com>
2024-11-11 21:43:06 +0100

    Add files via upload

--- Lab4/lab04.h
+++ Lab4/lab04.h
@@ -2,36 +2,82 @@
 #define COMPONENT_H
 #endif
 
+#include <string>
+#include <vector>
+
+class Connection {
+public:
+	double getPointVoltage() const {
+		return m_pointVoltage;
+	}
+	void changePointVoltage(double newVoltage);
+private:
+	double m_pointVoltage;
+};
+
 class Component {
 public:
-	virtual double computingVoltage() = 0;
-	virtual double computingCurrent() = 0;
+	Component() {//以防万一重载了一个默认的构造函数
+
+	}
+	Component(std::string componentName, Connection* leftConnection, Connection* rightConnection) :
+		m_componentName(componentName), m_leftConnection(leftConnection), m_rightConnection(rightConnection), m_current(0) {
+		
+	}
+	virtual void computingVoltage() = 0;//计算电压值，在函数内修改相应点的电压
+	virtual void computingCurrent() = 0;//计算当前元件的电流
 protected:
-	double m_terminal_1;
-	double m_terminal_2;
+	std::string		m_componentName;
+	Connection*		m_leftConnection;
+	Connection*		m_rightConnection;
+	double			m_current;
 };
 
 class Battery: public Component {
 public:
-	double computingVoltage() override;
-	double computingCurrent() override;
+	Battery() {
+
+	}
+	Battery(std::string batteryName, double voltage, Connection* leftConnection, Connection* rightConnection) :
+		Component(batteryName, leftConnection, rightConnection), m_voltage(voltage) {
+
+	}
+	void computingCurrent() override;
 private:
 	double m_voltage;
 };
 
 class Resistor : public Component {
 public:
-	double computingVoltage() override;
-	double computingCurrent() override;
+	Resistor() {
+
+	}
+	Resistor(std::string resistorName, double resistance, Connection* leftConnection, Connection* rightConnection) :
+		Component(resistorName, leftConnection, rightConnection), m_resistance(resistance) {
+
+	}
+	void computingVoltage() override;
+	void computingCurrent() override;
 private:
 	double m_resistance;
 };
 
 class Capacitor : public Component {
 public:
-	double computingVoltage() override;
-	double computingCurrent() override;
+	Capacitor() {
+
+	}
+	Capacitor(std::string capacitorName, double capacitance, Connection* leftConnection, Connection* rightConnection) :
+		Component(capacitorName, leftConnection, rightConnection), m_capacitance(capacitance), m_storedVoltage(0) {
+
+	}
+	void computingVoltage() override;//原名不能添加参数 只能用重载
+	void computingVoltage(double timeUnit);//重载，在里面修改m_storedVoltage
+	void computingCurrent() override;
 private:
 	double m_capacitance;
 	double m_storedVoltage;
 };
+
+void simulate(std::vector<Component*> circuit, int iterationTimes, int linesToPrint, int timeSlot);//循环计算的同时打印数据
+void deallocate_components(std::vector<Component*> circuit);//在这里delete vector里new的元件
