3058f6fde2eff75360a90d1613b29ae0e734b58b
ketjuk <74028692+ketjuk@users.noreply.github.com>
2024-11-12 13:26:56 +0100

    Add files via upload

--- Lab4/lab04.cpp
+++ Lab4/lab04.cpp
@@ -4,14 +4,14 @@
 #include <iomanip>
 
 void Resistor::computingVoltage(double timeSlot) {
-	double voltageDifference	= m_rightConnection->getPointVoltage() - m_leftConnection->getPointVoltage();	// 电势差
+	double voltageDifference	= m_leftConnection->getPointVoltage() - m_rightConnection->getPointVoltage();	// 电势差
 	double chargeMove			= voltageDifference / timeSlot;													// 电荷移动（用于leftConnection和rightConnection的电压更新）
 	m_leftConnection	->	changePointVoltage(m_leftConnection->getPointVoltage() - chargeMove);				// 电荷移动后，更新正极电压
 	m_rightConnection	->	changePointVoltage(m_rightConnection->getPointVoltage() + chargeMove);				// 电荷移动后，更新负极电压
 }
 
 void Resistor::computingCurrent() {
-	m_current = (m_rightConnection->getPointVoltage() - m_leftConnection->getPointVoltage()) / m_resistance;	// 使用（更新后的正极电压-更新后的负极电压）/电阻值
+	m_current = (m_leftConnection->getPointVoltage() - m_rightConnection->getPointVoltage()) / m_resistance;	// 使用（更新后的正极电压-更新后的负极电压）/电阻值
 																												// 获取当前的电流值
 }
 
