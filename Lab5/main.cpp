de78805fc3fc470d6cbdbb9b1d948c7988f6bf3b
Jerry <37730025+Johnnysharkhead@users.noreply.github.com>
2024-12-03 21:39:14 +0100

    add --frequency

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,8 +1,28 @@
 #include "lab05.h"
 #include <fstream>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <map>
+#include <iomanip> 
 
-int main(int argc, char* argv[]) {//argv[0]=main.exe argv[1]=any.txt argv[2]=parameter
+
+// Record " word : number of this word " by map container
+std::map <std::string, int> countWordFrequency( const std::vector<std::string> &words){
+    std::map <std::string, int> wordCount; // Intitialize a map
+    for(const auto &word : words){
+        ++wordCount[word]; // key is word and value is the number of the word
+    }
+    return wordCount; 
+}
+
+// Sort pairs in descending order of frequency occurrence
+bool sortByValue(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b){
+    return a.second > b.second;
+}
+
+int main(int argc, char* argv[]) { // argv[0]=main.exe argv[1]=any.txt argv[2]=parameter
 	if (argc < 2) {
 		std::cout << "file name is needed" << std::endl;
 		return 0;
@@ -17,8 +37,11 @@ int main(int argc, char* argv[]) {//argv[0]=main.exe argv[1]=any.txt argv[2]=par
 		std::cout << "cannot open the file" << std::endl;
 		return 1;
 	}
+	
 	std::string word;
+	
 	std::vector<std::string> text;//text is saved in vector<std::string>
+	
 	while (file >> word) {
 		text.push_back(word);
 	}
@@ -28,9 +51,24 @@ int main(int argc, char* argv[]) {//argv[0]=main.exe argv[1]=any.txt argv[2]=par
 		if (it.find("--print") != std::string::npos) {
 
 		}
+		
 		else if (it.find("--frequency") != std::string::npos) {
+			auto result = countWordFrequency(text);
 
+			// Copy all elements in map to a vector since we cannot sort anything in a map
+			// Get a vector containing all the map elements, each of which is a pair, preserving the original key-value relationships
+			std::vector < std::pair<std::string, int> > frequencyOrdered (result.begin(), result.end());
+
+			// Sort by the value in each element(pair) of the vector
+			std::sort(frequencyOrdered.begin(), frequencyOrdered.end(), sortByValue);
+
+			// Print all elements and keep the key (pair.first) right aligned
+			for (auto &pair : frequencyOrdered){
+				int width = 12;
+				std::cout << std::setw(width) << std::right << pair.first << " " << pair.second << std::endl;
+			}
 		}
+
 		else if (it.find("--table") != std::string::npos) {
 
 		}
