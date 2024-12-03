8e3e8e69a0394991ace726c0b260c8b54727a434
Jerry <37730025+Johnnysharkhead@users.noreply.github.com>
2024-12-03 11:32:04 +0100

    Follow the complementary work feedback, updated lab04.cpp & lab04.h

---
+++ Lab4/[tdde18_ht24 redovisning] lab4/lab04.cpp
@@ -0,0 +1,127 @@
+﻿#include "lab04.h"
+#include <iostream>
+#include <iomanip>
+
+
+//hint: Start with the following part, since some code will have to be rewritten:
+// TODO: Complementary work neeed: Do not repeat similar code. 
+// hint: computingVoltage contains a lot of repeat code.
+//       The cause of this is probably since your connection class is pretty complicated
+//		 e.g using only a double is sufficient in the connection class without "whichend()"
+//       you will probably have to rethink some parts of the implementation to solve this. -- CHECK 
+
+
+// TODO: Complementary work: Use '{...}' instead of '=' to initialize variables. -- CHECK
+
+// Comment: Instead of manually calling getVoltage and getCurrent when  
+// printing, implement a operator<< for Component  -- CHECK
+
+
+double Connection::getPointVoltage() const {
+	return m_pointVoltage;
+}
+
+void Connection::changePointVoltage(double newVoltage) {
+	if (!m_isFixed) { // Determine whether the connection is positive or negative, if yes, then the voltage remains constant
+        m_pointVoltage = newVoltage;
+    }
+}
+
+std::ostream& operator << (std::ostream& os, const Component& component) {
+    os << component.getVoltage()<< " " << component.getCurrent() << " ";
+    return os;
+}
+
+
+std::string Battery::getComponentName() const {
+	return m_componentName;
+}
+
+double Battery::getCurrent() const {
+	return 0.0;
+}
+
+double Battery::getVoltage() const {
+	return m_voltage;
+}
+
+void Battery::computingVoltage(double) {}
+
+void Battery::computingCurrent() {}
+
+
+std::string Resistor::getComponentName() const {
+	return m_componentName;
+}
+
+double Resistor::getCurrent() const {
+	return m_current;
+}
+
+void Resistor::computingVoltage(double timeSlot) {
+	double chargeMove = (this->getVoltage() / m_resistance) * timeSlot;
+	m_leftConnection->changePointVoltage(m_leftConnection->getPointVoltage() - chargeMove);
+	m_rightConnection->changePointVoltage(m_rightConnection->getPointVoltage() + chargeMove);
+}
+
+void Resistor::computingCurrent() {
+	m_current = getVoltage() / m_resistance;
+}
+
+
+std::string Capacitor::getComponentName() const {
+	return m_componentName;
+}
+
+double Capacitor::getCurrent() const {
+	return m_current;
+}
+
+void Capacitor::computingVoltage(double timeSlot) {
+	double chargeToStore = m_capacitance * (this->getVoltage() - m_storedVoltage);
+	m_leftConnection->changePointVoltage(m_leftConnection->getPointVoltage() - chargeToStore);
+	m_rightConnection->changePointVoltage(m_rightConnection->getPointVoltage() + chargeToStore);
+	m_storedVoltage = m_storedVoltage + chargeToStore; // Used to calculate the current of capacitor
+}
+
+void Capacitor::computingCurrent() {
+	m_current = m_capacitance * (m_leftConnection->getPointVoltage() - m_rightConnection->getPointVoltage() - m_storedVoltage);
+	if (m_current < 0.0) m_current = -(m_current);
+}
+
+
+void simulate(std::vector<Component*>& circuit, int iterationTimes, int linesToPrint, double timeSlot) {
+	int iterationTimesBeforePrint = iterationTimes / linesToPrint;
+	for (auto i : circuit) {
+		std::cout << std::right << std::setw(11) << i->getComponentName();
+	}
+	std::cout << std::endl;
+	for (auto i : circuit) {
+		std::cout << std::right << std::setw(11) << "Volt Curr";
+	}
+	std::cout << std::endl;
+
+	int count {0};
+	while (count < iterationTimes && linesToPrint > 0) {
+		count++;
+		for (auto i : circuit) {
+			i->computingVoltage(timeSlot);
+			i->computingCurrent();
+		}
+		if (count == iterationTimesBeforePrint) {
+			count = 0;
+			linesToPrint--;
+			for (auto i : circuit) {
+				std::cout << std::right << std::setw(5) << std::fixed << std::setprecision(2) << *i;
+			}
+			std::cout << std::endl;
+		}
+	}
+}
+
+void deallocate_components(std::vector<Component*>& circuit) {
+	for (auto i : circuit) {
+		delete i;
+	}
+	circuit.clear();
+}
---
+++ Lab4/[tdde18_ht24 redovisning] lab4/lab04.h
@@ -0,0 +1,111 @@
+﻿#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <string>
+#include <vector>
+
+// Complementary work: Define is c code. This is a c++ course. You enum is more appropriate.
+// 		But look into the comments in the other file first about replacing the connection component.-- CHECK
+
+// TODO: Complementary work: Parameters to functions that you don’t use  
+// should be declared without a name to signal that they are unused. -- CHECK
+
+class Connection {
+public:
+	Connection(): m_pointVoltage{0}, m_isFixed{false} {} // Constructor(Default)
+	Connection(double voltage, bool isFixed = false): m_pointVoltage{voltage}, m_isFixed{isFixed} {} // Constructor(used to initialise positive and negative terminals)
+
+	double getPointVoltage() const; // Get the current voltage value of this connection point
+	void changePointVoltage(double newVoltage); // Calculate and update the value of the voltage at this connection point after the change with charge movement
+
+private:
+	double m_pointVoltage;
+	bool m_isFixed; 
+};
+
+
+// The abstract base class for all components in this lab
+class Component {
+public:
+	Component() {}
+	Component(std::string componentName, Connection* leftConnection, Connection* rightConnection) :
+		m_componentName(componentName),	m_leftConnection(leftConnection), m_rightConnection(rightConnection), m_current(0) {}
+	
+	virtual ~Component() = default; 
+
+	virtual std::string	getComponentName() const = 0;
+	virtual double getCurrent()	const = 0; // Get the value of current going through the component	
+	virtual double getVoltage()	const{ // Get the voltage at the left connection point of the component minus the voltage at the right connection point
+		return m_leftConnection->getPointVoltage() - m_rightConnection->getPointVoltage();
+	}; 
+
+	virtual void computingVoltage(double /*timeSlot*/) = 0; // Involves the movement and storage of charges, and updating the voltage values at the left and right connection points of the component respectively
+	virtual void computingCurrent() = 0;
+
+	friend std::ostream& operator << (std::ostream&, const Component &component);
+
+protected:
+	std::string m_componentName;
+	Connection*	m_leftConnection; // A pointer that point to an object(the left connection) of the Connection class
+	Connection*	m_rightConnection; // A pointer that point to an object of the Connection class
+	double m_current;
+};
+
+class Battery : public Component {
+public:
+	Battery(): m_voltage(0) {}
+
+	Battery(std::string batteryName, double voltage, Connection* leftConnection, Connection* rightConnection) :
+		Component{batteryName, leftConnection, rightConnection}, m_voltage{voltage}{
+		this->m_leftConnection->changePointVoltage(m_voltage); // Assign the voltage of the left connection point of the battery directly to the voltage value of the battery in this Constructor.
+		}
+
+	std::string getComponentName() const override;
+	double getCurrent() const override;
+	double getVoltage()	const override;
+
+	void computingVoltage(double) override;
+	void computingCurrent() override;
+
+private:
+	double const m_voltage;
+};
+
+class Resistor : public Component {
+public:
+	Resistor() : m_resistance(0) {}
+	Resistor(std::string resistorName, double resistance, Connection* leftConnection, Connection* rightConnection) :
+		Component{resistorName,	leftConnection,	rightConnection}, m_resistance(resistance) {}
+	
+	
+	std::string	getComponentName() const override;
+	double getCurrent() const override;
+
+	void computingVoltage(double timeSlot) override;
+	void computingCurrent()	override;
+
+private:
+	double const m_resistance;
+};
+
+class Capacitor : public Component {
+public:
+	Capacitor() : m_capacitance(0) {}
+	Capacitor(std::string capacitorName, double capacitance, Connection* leftConnection, Connection* rightConnection) :
+		Component{capacitorName, leftConnection, rightConnection}, m_capacitance{capacitance}, m_storedVoltage{0} {}
+
+	std::string getComponentName() const override;
+	double getCurrent() const override;
+
+	void computingVoltage(double timeSlot) override;
+	void computingCurrent() override;
+
+private:
+	double const m_capacitance;
+	double m_storedVoltage; 
+};
+
+void simulate(std::vector<Component*>& circuit, int iterationTimes, int linesToPrint, double timeSlot); // Analogue circuit simulation where the circuit reaches steady state after a certain period of time
+void deallocate_components(std::vector<Component*>& circuit); // Release the object pointed by the pointer in the heap space to avoid memory leaks.
+
+#endif
