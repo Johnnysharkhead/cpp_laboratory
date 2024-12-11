5b383c94bacabc5ac252c61dde7123be1b676fa5
Jerry <37730025+Johnnysharkhead@users.noreply.github.com>
2024-12-11 20:51:05 +0100

    Using the STL algorithms to implement "--remove" and "--substitute"

--- Lab5/lab05.h
+++ Lab5/lab05.h
@@ -11,39 +11,6 @@
 #include <map>
 #include <iomanip> 
 
-// Get the longest word in a vector, using its length in setting the width of first column regarding the output 
-int firstcolumnWidth(const std::vector<std::string> vec) {
-	std::string longestWord {};
-	for(auto const &word : vec){
-	if (word.length() > longestWord.length()) {
-		longestWord = word;
-		}
-	}
-	std::cout << "The longest word in the text is: " << longestWord << ", with the length of " << longestWord.length() << std::endl;
-	std::cout << "So we set the width of the first column: " << longestWord.length() << std::endl; 
-	return longestWord.length();
-}
-	
-
-// Record " word : number of this word " by map container
-std::map <std::string, int> countWordFrequency( const std::vector<std::string> &words){  //the parameter is vector with all words stored
-    
-	std::map <std::string, int> wordCount; // Initialize a map named wordCount
-	// std::map < std::string, int, std::greater<std::string> > wordCount;
-    
-	for(const auto &word : words){
-        ++wordCount[word]; // key is word(the way to access a element in a map) and value is the number of the word
-    }
-    return wordCount; // By default, this map is sorted by **key** in ascending order.
-}
-
-// Sort pairs in descending order of frequency occurrence
-bool sortByValue(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b){
-    return a.second > b.second; // .second = [the value of each pair], namely the occurance frequency for each word
-	//return a.second < b.second;
-}
-
-
 //enter a single word, returns the word with all lower case letters
 std::string toLowerCase(const std::string& str) {
 	std::string result = str;
@@ -51,37 +18,51 @@ std::string toLowerCase(const std::string& str) {
 	return result;
 }
 
-void substitudeText(std::vector<std::string>& text,const std::string& oldWord, const std::string& newWord) {
-	for (auto& it : text) {
-		if (toLowerCase(it) == toLowerCase(oldWord)) {
-			it = newWord;
-			break;
-		}
-	}
-	std:: cout << "The word you input is not in text, please input the word that you want to replace again." << std::endl;
-}
+// // Get the longest word in a vector, using its length in setting the width of first column regarding the output 
+// int firstcolumnWidth(const std::vector<std::string> vec) {
+// 	std::string longestWord {};
+// 	for(auto const &word : vec){
+// 		if (word.length() > longestWord.length()) {
+// 			longestWord = word;
+// 		}
+// 	}
+// 	std::cout << "The longest word in the text is: " << longestWord << ", with the length of " << longestWord.length() << std::endl;
+// 	std::cout << "So we set the width of the first column: " << longestWord.length() << std::endl; 
+// 	return longestWord.length();
+// }
 
-void removeText(std::vector<std::string>& text, const std::string& word) {
-	auto it = text.begin();
-	bool flag;
-	while (it != text.end()) {
-		if (toLowerCase(*it) == toLowerCase(word)) {
-			it = text.erase(it);
-			flag = false;
-		}
-		else it++;
-	}
-	if(flag){
-		std:: cout << "The word you input is not in text, please input the word that you want to delete again." << std::endl;
-	}
+// // Sort pairs in descending order of frequency occurrence
+// bool sortByValue(const std::pair<std::string, int> &a, const std::pair<std::string, int> &b){
+//     return a.second > b.second; // .second = [the value of each pair], namely the occurance frequency for each word
+// 	//return a.second < b.second;
+// }
+
+
+// void substitudeText(std::vector<std::string>& text,const std::string& oldWord, const std::string& newWord) {
+// 	for (auto& it : text) {
+// 		if (toLowerCase(it) == toLowerCase(oldWord)) {
+// 			it = newWord;
+// 			break;
+// 		}
+// 	}
+// 	std:: cout << "The word you input is not in text, please input the word that you want to replace again." << std::endl;
+// }
+
+// void removeText(std::vector<std::string>& text, const std::string& word) {
+// 	auto it = text.begin();
+// 	bool flag;
+// 	while (it != text.end()) {
+// 		if (toLowerCase(*it) == toLowerCase(word)) {
+// 			it = text.erase(it);
+// 			flag = false;
+// 		}
+// 		else it++;
+// 	}
+// 	if(flag){
+// 		std:: cout << "The word you input is not in text, please input the word that you want to delete again." << std::endl;
+// 	}
 	
-}
-
-
-
-
-
-
+// }
 
 
 #endif
--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -6,8 +6,21 @@
 #include <vector>
 #include <map>
 #include <iomanip> 
+#include <cmath>
 
-		
+// Transfer the vector with all words stored to a map 
+// Record " word : number of this word " by map container, which is used in "--frequency" and "--table"
+std::map <std::string, int> countWordFrequency( const std::vector<std::string> &words){ 
+	
+	std::map <std::string, int> wordCount; // Initialize a map named wordCount
+	// std::map < std::string, int, std::greater<std::string> > wordCount;
+	for(const auto &word : words){
+        ++wordCount[word]; // key is word(the way to access a element in a map) and value is the number of the word
+    }
+    return wordCount; // By default, this map is sorted by **key** in ascending order.
+
+}
+
 
 int main(int argc, char* argv[]) { // argv[0]=main.exe argv[1]=any.txt argv[2]=parameter
 	if (argc < 2) {
@@ -31,6 +44,15 @@ int main(int argc, char* argv[]) { // argv[0]=main.exe argv[1]=any.txt argv[2]=p
 		text.push_back(word);
 	}
 
+	// Get the longest word in a vector, using its length in setting the width of first column regarding the output 
+	auto it = std::max_element(text.begin(), text.end(), [](std::string a, std::string b){
+		return a.length() < b.length();
+	});
+	int columnWidth = (*it).length();
+	// std::cout << "The longest word in the text is: " << *it << ", with the length of " << columnWidth << std::endl;
+	// std::cout << "So we set the width of the first column: " << columnWidth << std::endl; 
+
+
 	std::vector<std::string> parameters(argv + 2, argv + argc);//parameters are saved in vector<std::string>
 	
 	for (auto it : parameters) {
@@ -42,7 +64,7 @@ int main(int argc, char* argv[]) { // argv[0]=main.exe argv[1]=any.txt argv[2]=p
 			);
 			std::cout << std::endl;
 
-			// Alternative way (using iterator) to implment print
+			// // Alternative way (using iterator) to implment print
 			// for(auto it = text.begin(); it != text.end(); ++it){
 			// 	std::cout << *it << " ";
 			// }
@@ -58,45 +80,58 @@ int main(int argc, char* argv[]) { // argv[0]=main.exe argv[1]=any.txt argv[2]=p
 			// Get a vector(frequencyOrdered) containing all the map elements, each of which is a pair, preserving the original key-value relationships
 			std::vector < std::pair<std::string, int> > frequencyOrdered (result.begin(), result.end());
 
-			// Sort by the value in each element(pair) of the vector
-			std::sort(frequencyOrdered.begin(), frequencyOrdered.end(), sortByValue);
-			
+			// Sort by the value of pairs in a vector in descending/ascending order of frequency occurrence
+			std::sort(frequencyOrdered.begin(), frequencyOrdered.end(), 
+				[](const std::pair<std::string, int> &a, const std::pair<std::string, int> &b){
+				return a.second > b.second;
+				//return a.sencond < b.second;
+				});
+
 			// Print all elements and keep the key (pair.first) right aligned
-			int columnWidth = firstcolumnWidth(text);
 			for (auto &pair : frequencyOrdered){
 				std::cout << std::setw(columnWidth) << std::right << pair.first << " " << pair.second << std::endl;
 			}
 		}
 
-
 		else if (it.find("--table") != std::string::npos) {
 			auto result = countWordFrequency(text); // By default, map in C++ is sorted by **key** in ascending order.
 			
-			int columnWidth = firstcolumnWidth(text);
-			
-			std::for_each(
-				result.begin(), result.end(),[&columnWidth](const std::pair<std::string,int> &temp){
+			std::for_each( // The elements in the vector(result) are type "pair<string, int>".
+				result.begin(), result.end(),[&columnWidth](const std::pair<std::string,int> &temp){ // Capture the width of column in lambda function
 					std::cout << std::setw(columnWidth) << std::left << temp.first << " " << temp.second << std::endl;
 				}
 			);
 		}
-
 
 		else if (it.find("--substitute=") != std::string::npos) {
 			auto equalSign = it.find('=');
 			auto plusSign = it.find('+');
 			std::string oldWord = it.substr(equalSign + 1, plusSign - equalSign - 1);
 			std::string newWord = it.substr(plusSign + 1);
-			substitudeText(text, oldWord, newWord);
+			
+			auto findIT = std::find(text.begin(), text.end(), oldWord); // a iterator will move to the word that you want to replace 
+			if(findIT != text.end()){
+				std::replace(text.begin(), text.end(), oldWord, newWord);
+			}
+			else{
+				std:: cout << "The word you input is not in text, please input the word that you want to replace again." << std::endl;
+			}	
 		}
 
 		else if(it.find("--remove=") != std::string::npos) {
 			auto equalSign = it.find('=');
 			std::string newWord = it.substr(equalSign + 1);
-			removeText(text, newWord);
-		}
+
+			auto findIT = std::find(text.begin(), text.end(), newWord);
+			if(findIT != text.end()){
+				text.erase( std::remove_if(text.begin(), text.end(), [&newWord] (std::string x) { return x == newWord; }), text.end() );
+			}
+			else{
+				std:: cout << "The word you input is not in text, please input the word that you want to delete again." << std::endl;
+			}
+		}	
+
 	}
 
-	//we may not need to write it back to the file yet?
 	return 0;
 }
